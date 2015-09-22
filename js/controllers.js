13f8f2a00510ad6edf046d65f3028905f0398e9b
J R Bennett <justinbennett@outlook.com>
2015-09-22 10:29:38 -0400

    Added

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,29 @@
 var ToDoApp = angular.module('ToDoApp', []);
 
 ToDoApp.controller('ToDoCtrl', function ($scope) {
-  $scope.tasks = [
-      {item: "Give Justin an A", done: false, priority: true},
-      {item: "Feed the cat", done: false, priority: false},
-      {item: "Cut the lawn", done: false, priority: false}
-  ];
+      $scope.tasks = [
+          {item: "Give Justin an A", done: false, priority: true},
+          {item: "Feed the cat", done: false, priority: false},
+          {item: "Cut the lawn", done: false, priority: false}
+      ];
+    
+    $scope.setfocus = function() {
+        document.getElementById('inputToDo').focus();
+    };
 });
 
 ToDoApp.controller('TaskSubmitCtrl', function($scope) {
     $scope.submit = function() {
         $scope.tasks.push({item: $scope.taskDescrip, done: false});
         $scope.taskDescrip = '';
+        $scope.setfocus();
     };
-  console.log($scope.tasks);
 });
 
 ToDoApp.controller('TaskDelete', function($scope) {
     $scope.delete = function(idx) {
-      console.log(idx);
         $scope.tasks.splice(idx, 1);
+        $scope.setfocus();
     };
 });
+
